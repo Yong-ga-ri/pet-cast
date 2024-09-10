7a120d1554c945809f9dbe891d65fbfd9a78fb57
gilhan <rlfgks97@gmail.com>
2024-09-10 23:58:50 +0900

    refactor: deactivate servlet filtering

--- backend/src/main/java/com/varchar6/petcast/security/filter/TokenFilter.java
+++ backend/src/main/java/com/varchar6/petcast/security/filter/TokenFilter.java
@@ -19,27 +19,27 @@ public class TokenFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = ((HttpServletRequest) servletRequest);
-        String authorizationHeader = httpServletRequest.getHeader("Authorization");
-
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String token = authorizationHeader.replace("Bearer ", "");
-            Authentication authentication = jwtUtil.getAuthentication(token);
-            if (authentication != null) {
-                CustomUser customUser = (CustomUser) authentication.getPrincipal();
-                httpServletRequest.setAttribute("memberId", customUser.getId());
-                httpServletRequest.setAttribute("memberLoginId", customUser.getUsername());
-                httpServletRequest.setAttribute("memberName", customUser.getName());
-                httpServletRequest.setAttribute("memberPhone", customUser.getPhone());
-                httpServletRequest.setAttribute("memberNickname", customUser.getNickname());
-                httpServletRequest.setAttribute("image", customUser.getImage());
-                httpServletRequest.setAttribute("created", customUser.getCreatedAt());
-                httpServletRequest.setAttribute("updated", customUser.getUpdatedAt());
-                httpServletRequest.setAttribute("active", customUser.isActive());
-                httpServletRequest.setAttribute("introduction", customUser.getIntroduction());
-                httpServletRequest.setAttribute("authorities", customUser.getAuthorities());
-            }
-        }
+//        HttpServletRequest httpServletRequest = ((HttpServletRequest) servletRequest);
+//        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+//
+//        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+//            String token = authorizationHeader.replace("Bearer ", "");
+//            Authentication authentication = jwtUtil.getAuthentication(token);
+//            if (authentication != null) {
+//                CustomUser customUser = (CustomUser) authentication.getPrincipal();
+//                httpServletRequest.setAttribute("memberId", customUser.getId());
+//                httpServletRequest.setAttribute("memberLoginId", customUser.getUsername());
+//                httpServletRequest.setAttribute("memberName", customUser.getName());
+//                httpServletRequest.setAttribute("memberPhone", customUser.getPhone());
+//                httpServletRequest.setAttribute("memberNickname", customUser.getNickname());
+//                httpServletRequest.setAttribute("image", customUser.getImage());
+//                httpServletRequest.setAttribute("created", customUser.getCreatedAt());
+//                httpServletRequest.setAttribute("updated", customUser.getUpdatedAt());
+//                httpServletRequest.setAttribute("active", customUser.isActive());
+//                httpServletRequest.setAttribute("introduction", customUser.getIntroduction());
+//                httpServletRequest.setAttribute("authorities", customUser.getAuthorities());
+//            }
+//        }
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
