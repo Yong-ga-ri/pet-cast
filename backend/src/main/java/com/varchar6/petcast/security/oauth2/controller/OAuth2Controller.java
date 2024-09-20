00320b8198e7633e7cb5350fab46dfa019fad579
gilhan <rlfgks97@gmail.com>
2024-09-20 11:01:24 +0900

    feat: add filter for specification

--- backend/src/main/java/com/varchar6/petcast/security/oauth2/controller/OAuth2Controller.java
+++ backend/src/main/java/com/varchar6/petcast/security/oauth2/controller/OAuth2Controller.java
@@ -6,16 +6,12 @@ import com.varchar6.petcast.security.oauth2.vo.google.GoogleUserInformationVO;
 import com.varchar6.petcast.security.oauth2.vo.naver.NaverUserInformationVO;
 import com.varchar6.petcast.security.oauth2.vo.responsetoken.KakaoOAuth2TokenResponseVO;
 import com.varchar6.petcast.security.oauth2.vo.kakao.KakaoUserInformationVO;
-import com.varchar6.petcast.security.oauth2.vo.responsetoken.NaverOAuth2TokenResponseVO;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestController
